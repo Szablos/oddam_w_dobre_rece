bf7b995485dbd83f3318553d0a26d0bc761d12ad
Filip <filszab@gmail.com>
2019-07-14 23:03:31 +0200

    about done

--- src/components/FourSteps.js
+++ src/components/FourSteps.js
@@ -8,8 +8,31 @@ const FourSteps = () => {
       <div className="four_steps_container">
         <p>Wystarczą 4 proste kroki</p>
         <img src={decoration} alt="" />
-        <div>
-
+        <div className="icons-container">
+          <div className="icon">
+            <i className="fas fa-clipboard-list"></i>
+            <p>Wybierz rzeczy</p>
+            <hr></hr>
+            <p>ubrania, zabawki, sprzęt i inne</p>
+          </div>
+          <div className="icon">
+            <i className="fas fa-arrow-down"></i>
+            <p>Spakuj je</p>
+            <hr></hr>
+            <p>skorzystaj z worków na śmieci</p>
+          </div>
+          <div className="icon">
+            <i className="fas fa-binoculars"></i>
+            <p>Zdecyduj komu chcesz pomóc</p>
+            <hr></hr>
+            <p>wybierz zaufane miejsce</p>
+          </div>
+          <div className="icon">
+            <i className="fas fa-box-open"></i>
+            <p>Zamów kuriera</p>
+            <hr></hr>
+            <p>kurier przyjedzie w dogodnym terminie</p>
+          </div>
         </div>
         <button>Załóz konto</button>
       </div>
